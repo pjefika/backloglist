db4dc7db4bbdb43d531ce281929873be31118208
G0034481 <G0034481@CTARO8-5001.gvt.net.br>
2016-06-17 08:57:02 -0300

    Aumentado capacidate e quantidade do fulltest para 4(quatro)

--- backloglist/JavaSource/controllers/FulltestBean.java
+++ backloglist/JavaSource/controllers/FulltestBean.java
@@ -21,6 +21,8 @@ public class FulltestBean {
 
 	Timer timerFulltestDefeitosImportados1 = new Timer();
 	Timer timerFulltestDefeitosImportados2 = new Timer();
+	Timer timerFulltestDefeitosImportados3 = new Timer();
+	Timer timerFulltestDefeitosImportados4 = new Timer();
 	Timer timerExcluiDefeitosParados = new Timer();
 
 	TimerTask FulltestDefeitosImportados1 = new TimerTask() {
@@ -58,7 +60,43 @@ public class FulltestBean {
 
 		}
 	};
-	
+
+	TimerTask FulltestDefeitosImportados3 = new TimerTask() {
+
+		@Override
+		public void run() {
+
+			try {
+
+				consultaSS3();
+
+			} catch (NullPointerException e) {
+
+				//System.out.println(e.getMessage());
+
+			}
+
+		}
+	};
+
+	TimerTask FulltestDefeitosImportados4 = new TimerTask() {
+
+		@Override
+		public void run() {
+
+			try {
+
+				consultaSS4();
+
+			} catch (NullPointerException e) {
+
+				//System.out.println(e.getMessage());
+
+			}
+
+		}
+	};
+
 	TimerTask ExcluiDefeitosParados = new TimerTask() {
 
 		@Override
@@ -88,6 +126,8 @@ public class FulltestBean {
 
 		timerFulltestDefeitosImportados1.scheduleAtFixedRate(FulltestDefeitosImportados1, 1000, 1000);
 		timerFulltestDefeitosImportados2.scheduleAtFixedRate(FulltestDefeitosImportados2, 1200, 1200);
+		timerFulltestDefeitosImportados3.scheduleAtFixedRate(FulltestDefeitosImportados3, 1200, 1200);
+		timerFulltestDefeitosImportados4.scheduleAtFixedRate(FulltestDefeitosImportados4, 1200, 1200);
 		timerExcluiDefeitosParados.schedule(ExcluiDefeitosParados, 12000, 12000);
 
 	}
@@ -99,27 +139,27 @@ public class FulltestBean {
 	public void iniciaBean() {		
 
 	}	
-	
+
 	/**
 	 * Primeira Task
 	 **/
 
 	public void consultaSS1() throws NullPointerException{
-		
+
 		DefeitoIntegracao defeitosIntegracao1 = new DefeitoIntegracao();
 
 		try {
 
 			defeitosIntegracao1 = this.atendimentoServico.consultarSSIntegracao().get(0);
-						
+
 			this.atendimentoServico.consultarSS(defeitosIntegracao1.getSs());
 
 			this.importServicoNew.salvaLogIntegracao(defeitosIntegracao1, TipoLogIntegracao.DEFEITOEXISTENTE);
 
 		} catch (Exception e) {
-			
+
 			if (!defeitosIntegracao1.getInstancia().isEmpty()) {
-				
+
 				realizarFulltest1(defeitosIntegracao1);
 
 			}
@@ -141,32 +181,32 @@ public class FulltestBean {
 		}
 
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Segunda Task
 	 **/
 	public void consultaSS2() throws NullPointerException{
-		
+
 		DefeitoIntegracao defeitosIntegracao2 = new DefeitoIntegracao();
 
 		try {
 
 			defeitosIntegracao2 = atendimentoServico.consultarSSIntegracao().get(0);
-						
+
 			this.atendimentoServico.consultarSS(defeitosIntegracao2.getSs());
 
 			this.importServicoNew.salvaLogIntegracao(defeitosIntegracao2, TipoLogIntegracao.DEFEITOEXISTENTE);
 
 		} catch (Exception e) {
-			
+
 			if (!defeitosIntegracao2.getInstancia().isEmpty()) {
-								
+
 				realizarFulltest2(defeitosIntegracao2);
 
 			}
-			
+
 		}
 
 	}
@@ -184,11 +224,87 @@ public class FulltestBean {
 		}
 
 	}
+
+	public void consultaSS3() throws NullPointerException{
+
+		DefeitoIntegracao defeitosIntegracao3 = new DefeitoIntegracao();
+
+		try {
+
+			defeitosIntegracao3 = atendimentoServico.consultarSSIntegracao().get(0);
+
+			this.atendimentoServico.consultarSS(defeitosIntegracao3.getSs());
+
+			this.importServicoNew.salvaLogIntegracao(defeitosIntegracao3, TipoLogIntegracao.DEFEITOEXISTENTE);
+
+		} catch (Exception e) {
+
+			if (!defeitosIntegracao3.getInstancia().isEmpty()) {
+
+				realizarFulltest2(defeitosIntegracao3);
+
+			}
+
+		}
+
+	}
+
+	public void realizarFulltest3(DefeitoIntegracao defeitoIntegracao) {
+
+		try {
+			this.importServicoNew.trocaStatusDefeitoIntegracao(defeitoIntegracao);
+			this.importServicoNew.fulltest(defeitoIntegracao);
+
+		} catch (Exception e) {			
+
+			e.printStackTrace();
+
+		}
+
+	}	
 	
+	public void consultaSS4() throws NullPointerException{
+
+		DefeitoIntegracao defeitosIntegracao4 = new DefeitoIntegracao();
+
+		try {
+
+			defeitosIntegracao4 = atendimentoServico.consultarSSIntegracao().get(0);
+
+			this.atendimentoServico.consultarSS(defeitosIntegracao4.getSs());
+
+			this.importServicoNew.salvaLogIntegracao(defeitosIntegracao4, TipoLogIntegracao.DEFEITOEXISTENTE);
+
+		} catch (Exception e) {
+
+			if (!defeitosIntegracao4.getInstancia().isEmpty()) {
+
+				realizarFulltest2(defeitosIntegracao4);
+
+			}
+
+		}
+
+	}
+
+	public void realizarFulltest4(DefeitoIntegracao defeitoIntegracao) {
+
+		try {
+			this.importServicoNew.trocaStatusDefeitoIntegracao(defeitoIntegracao);
+			this.importServicoNew.fulltest(defeitoIntegracao);
+
+		} catch (Exception e) {			
+
+			e.printStackTrace();
+
+		}
+
+	}	
+
 	public void excluirDefeitoParado() {
-		
+
 		this.importServicoNew.removeDefeitosParados(this.importServicoNew.listaDefeitosParados());
-		
+
 	}
 
 }
